f3160d80c25e97e22877689d2193ba6dd88fcbee
plusdojo2024-iritani <iritani-shogo-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-14 17:57:08 +0900

    a

--- src/servlet/MachServlet.java
+++ src/servlet/MachServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.MachQuestionsDAO;
+import model.MachQuestions;
 
 /**
  * Servlet implementation class MachServlet
@@ -13,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/MachServlet")
 public class MachServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -28,6 +34,10 @@ public class MachServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		// 相性診断ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mach.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
@@ -36,6 +46,27 @@ public class MachServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+
+		// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				String q1 = request.getParameter("q1");
+				String q2 = request.getParameter("q2");
+				String q3 = request.getParameter("q3");
+				String q4 = request.getParameter("q4");
+				String q5 = request.getParameter("q5");
+
+				// 検索処理を行う
+				MachQuestionsDAO mqDao = new MachQuestionsDAO();
+				List<MachQuestions> cardList = mqDao.select(new MachQuestions());
+
+				// 検索結果をリクエストスコープに格納する
+				request.setAttribute("cardList", cardList);
+
+				// 結果ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mach_result.jsp");
+				dispatcher.forward(request, response);
+
+
 	}
 
 }
