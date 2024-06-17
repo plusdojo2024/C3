3037db28cceba915051239cbdf1058b906fbc896
plusdojo2024-mizutani <mizutani-yuka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 17:47:54 +0900

    6/17 水谷

--- src/servlet/CalendarServlet.java
+++ src/servlet/CalendarServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -36,6 +37,8 @@ public class CalendarServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
+
 	}
 
 }
