96801d5b80eb1413fadad45049f2a77862c83fe3
plusdojo2024-mizutani <mizutani-yuka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 14:33:51 +0900

    6/18 水谷

--- src/servlet/CalendarServlet.java
+++ src/servlet/CalendarServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,8 +13,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import dao.DmsDAO;
-
+import dao.BcDAO;
+import dao.EventsDAO;
+import dao.ReservationsDAO;
+import model.Bc;
+import model.Events;
+import model.Reservations;
+import model.Result;
 /**
  * Servlet implementation class CalendarServlet
  */
@@ -40,7 +48,7 @@ public class CalendarServlet extends HttpServlet {
 		// 登録ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/.jsp");
 		dispatcher.forward(request, response);
-	}
+		}
 
 
 	/**
@@ -50,12 +58,68 @@ public class CalendarServlet extends HttpServlet {
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
 		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/simpleBC/LoginServlet");
+			response.sendRedirect("/C3/UserServlet");
 			return;
 		}
 
+		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
-		DmsDAO dmsDao = new DmsDAO();
+		String Id = request.getParameter("Id");
+		int Id = Integer.parseInt(Id);
+		
+		String event_name = request.getParameter("event_name");
+		
+		String tempEvent_day = request.getParameter("event_day");
+		try {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = sdf.parse(tempEvent_day);
+		
+		String event_place = request.getParameter("event_place");
+		String event_remarks = request.getParameter("event_remarks");
+		String user_name = request.getParameter("user_name");
+		
+		
+		EventsDAO EvDao = new EventsDAO();
+		
+		// 登録
+		if (EvDao.insert(new Ev(0, event_name, event_day, event_place, event_remarks, user_name))) {	// 登録成功
+			// 改造（ここまで）
+			request.setAttribute("result",
+			new Result("登録成功", "レコードを1件登録しました。", "/C3/CalendarServlet"));
+		}
+		else {
+			request.setAttribute("result",
+			new Result("登録失敗", "レコードを登録できませんでした。", "/C3/CalendarServlet"));
+		}
+
+		
+		// 更新と削除
+		if (request.getParameter("submit").equals("更新")) { // submitでOK?
+			if (EvDao.update(new Ev(0, event_name, event_day, event_place, event_remarks, user_name))) {	// 更新成功
+				request.setAttribute("result",
+				new Result("更新成功", "レコードを1件更新しました。", "/C3/CalendarServlet"));
+			}
+			else {												// 更新失敗
+				request.setAttribute("result",
+				new Result("更新失敗しちゃいました…", "レコードを更新できませんでした。", "/C3/CalendarServlet"));
+			}
+		}
+		else {
+			if (EvDao.delete(id)) {
+				request.setAttribute("result",
+				new Result("削除成功", "レコードを1件削除しました。", "/C3/CalendarServlet"));
+			}
+			else {
+				request.setAttribute("result",
+				new Result("削除失敗", "レコードを削除できませんでした。", "/C3/CalendarServlet"));
+			}
+		}
+
+			
+		}
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/calendar.jsp");
+		dispatcher.forward(request, response);
 
 	}
 
