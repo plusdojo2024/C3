ec6a77f8a07535ad1e85b8e28dcc28c3bee48e6d
plusdojo2024-yamamoto <yamamoto-seiya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 13:40:50 +0900

    あ

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -35,8 +35,8 @@ public class LoginServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
-        id = "";
-        pw = "";
+        id = "orgA";
+        pw = "orgA";
 		//isLoginOKは使わない
 		//loginIDがあれば個人か団体か判断、なければnullをかえす
 		// ログイン処理を行う
