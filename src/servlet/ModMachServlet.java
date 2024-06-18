47af205a4da820941efdcc0bf77acfcdbf044ed0
plusdojo2024-mizutani <mizutani-yuka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 16:31:10 +0900

    6/18 水谷

--- src/servlet/ModMachServlet.java
+++ src/servlet/ModMachServlet.java
@@ -1,11 +1,19 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.MachQuestionsDAO;
+import model.MachQuestions;
+import model.Result;
 
 /**
  * Servlet implementation class ModMachServlet
@@ -26,16 +34,80 @@ public class ModMachServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/C3/ManegerServlet");
+			return;
+		}
+		// 検索ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mod_mach.jsp");
+		dispatcher.forward(request, response);
+		}
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+				HttpSession session = request.getSession();
+				if (session.getAttribute("id") == null) {
+					response.sendRedirect("/C3/ManegerServlet");
+					return;
+				
+				// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+				String tempa1 = request.getParameter("answer1");
+				String tempa2 = request.getParameter("answer2");
+				String tempa3 = request.getParameter("answer3");
+				String tempa4 = request.getParameter("answer4");
+				String tempa5 = request.getParameter("answer5");
 
+				// 検索処理を行う
+				
+				
+				// インスタンスを生成
+				MachQuestionsDao mqDao = new MachQuestionsDAO();
+				
+				List<String> mod_machList = mqDao.select(new select(a1, a2, a3, a4, a5));
+				request.setAttribute("cardList", cardList);
+
+				// 登録する
+				if (mqDao.insert(new Mq(a1, a2, a3, a4, a5))) {
+					request.setAttribute("result",
+					new Result("登録成功", "レコードを1件登録しました。", "/C3/ModMachServlet"));
+				}
+				else {
+					request.setAttribute("result",
+					new Result("登録失敗", "レコードを登録できませんでした。", "/C3/ModMuchServlet"));
+				}
+				
+				
+				// 更新・削除する
+				if (request.getParameter("submit").equals("更新")) { //submitでOK？
+					if (mqDao.update(new Mq(a1, a2, a3, a4, a5))) {
+						request.setAttribute("result",
+						new Result("更新成功", "レコードを1件更新しました。", "/C3/ModMachServlet"));
+					}
+					else {
+						request.setAttribute("result",
+						new Result("更新失敗", "レコードを更新できませんでした。", "/C3/ModMachServlet"));
+					}
+				}
+				else {
+					if (mqDao.delete(id)) {
+						request.setAttribute("result",
+						new Result("削除成功", "レコードを1件削除しました。", "/C3/ModMachServlet"));
+					}
+					else {
+						request.setAttribute("result",
+						new Result("削除失敗", "レコードを削除できませんでした。", "/C3/ModMachServlet"));
+					}
+				
+				
+				// 結果ページにフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mod_mach.jsp");
+				dispatcher.forward(request, response);
+	
 }
