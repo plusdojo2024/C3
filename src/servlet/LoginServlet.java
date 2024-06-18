6d7b1ea49b9e9ce43dec3a7300c29a5726ebc1e2
plusdojo2024-yamamoto <yamamoto-seiya-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 14:28:45 +0900

    あ

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -33,8 +33,8 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
-		String id = request.getParameter("id");
-		String pw = request.getParameter("pw");
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_password");
         id = "orgA";
         pw = "orgA";
 		//isLoginOKは使わない
@@ -49,6 +49,7 @@ public class LoginServlet extends HttpServlet {
 			for(Users loginlist: loginList) {
 			if(loginlist.getIs_organization()== true){
 //団体用のホームページ
+				System.out.println(loginlist.getIs_organization());
 				response.sendRedirect("/C3/GroupHomeServlet");
 			}
 			else if(loginlist.getIs_organization()== false) {
