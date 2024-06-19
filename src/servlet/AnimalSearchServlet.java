6c90f15803eacb9d63e5f3562e4f3ad26dde3b96
plusdojo2024-iritani <iritani-shogo-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 17:10:02 +0900

    s

--- src/servlet/AnimalSearchServlet.java
+++ src/servlet/AnimalSearchServlet.java
@@ -20,20 +20,11 @@ import model.Individuals;
 public class AnimalSearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AnimalSearchServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
 
 		// 検索ページにフォワードする
 				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/animal_search.jsp");
