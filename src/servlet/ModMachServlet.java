e4cf36773e0cb40fd25851fb417d6dd16e2dc666
plusdojo2024-kishida <kishida-shiho-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-21 17:20:19 +0900

    更新

--- src/servlet/ModMachServlet.java
+++ src/servlet/ModMachServlet.java
@@ -34,12 +34,9 @@ public class ModMachServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+
 		HttpSession session = request.getSession();
-		if (session.getAttribute("id") == null) {
-			response.sendRedirect("/C3/ManegerServlet");
-			return;
-		}
+
 		// 検索ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/mod_mach.jsp");
 		dispatcher.forward(request, response);
